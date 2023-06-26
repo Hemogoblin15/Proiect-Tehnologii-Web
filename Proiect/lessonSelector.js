b3fd411f4617ba8a66c7da7ea50b33be611ee5e3
Mihail Ghidoarcă <mihailghidoarca2002@gmail.com>
2023-06-26 05:16:58 +0300

    profile-add-edit-rss

--- Proiect/lessonSelector.js
+++
@@ -1,8 +0,0 @@
-// let selectedLessonTitle;
-
-// function setLessonTitle(lessonTitle) {
-//   selectedLessonNumber = lessonTitle;
-//   window.location.href = `/${lessonNumber}`; // Redirect to the specific lesson page
-// }
-
-// module.exports = { setLessonNumber };
